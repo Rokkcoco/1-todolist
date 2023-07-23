a732c22d947809f7a5e08d569255dae2ad98914b
Evgeniy Ivanov <p8635241@gmail.com>
2023-07-23 12:11:49 +0500

    new stories for todolists api

--- src/stories/todolists-api.stories.tsx
+++ src/stories/todolists-api.stories.tsx
@@ -29,3 +29,30 @@ export const CreateTodolist = () => {
 
     return <div>{JSON.stringify(state)}</div>
 }
+export const DeleteTodolist = () => {
+    const [state, setState] = useState<any>(null)
+    const todolistID = "0f873401-b5d3-4199-9c45-eca5a6556cd5"
+
+    useEffect(() => {
+        todoListApi.deleteTodo(todolistID)
+            .then((res) => {
+                setState(res.data)
+            })
+    }, [])
+
+    return <div>{JSON.stringify(state)}</div>
+}
+export const UpdateTodolistTitle = () => {
+    const [state, setState] = useState<any>(null)
+    const todolistID = "3f8515d3-314d-4622-b498-6728336a98c3"
+    const title = "JS"
+
+    useEffect(() => {
+        todoListApi.updateTodo(todolistID, title)
+            .then((res) => {
+                setState(res.data)
+            })
+    }, [])
+
+    return <div>{JSON.stringify(state)}</div>
+}
